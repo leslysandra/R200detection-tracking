c89f0571ca193fc629e0c7054042b6df1833a9c3
l3s777 <lesly.zerna.bo07@gmail.com>
2016-10-25 20:08:25 +0200

    more comments for a better and readable code

--- gnuplot/gp.sh
+++ gnuplot/gp.sh
@@ -1,1 +1,2 @@
-gnuplot -e "input_file='/home/aerolabio/librealsense/examples/librealsense_feat/gnuplot/pos-time.csv'" /home/aerolabio/librealsense/examples/librealsense_feat/gnuplot/gp_timeStats_popup.pg
+#gnuplot -e "input_file='/home/aerolabio/librealsense/examples/librealsense_feat/gnuplot/pos-time.csv'" /home/aerolabio/librealsense/examples/librealsense_feat/gnuplot/gp_timeStats_popup.pg
+gnuplot -e "input_file='$1'" gp_timeStats_popup.pg
--- src/main.cpp
+++ src/main.cpp
@@ -42,6 +42,9 @@
 #include "utils.h"
 #include "common.h"
 
+using namespace std;
+using namespace cv;
+
 /* HSV space variables for PURPLE blob detection */
 int hMin = 120;
 int sMin = 148;
@@ -49,25 +52,27 @@ int vMin = 55;
 int hMax = 256;
 int sMax = 256;
 int vMax = 130;
-/* ... */
 
+// threshold value
 int thresh = 100;
 
-
-using namespace std;
-using namespace cv;
-
+// writing into external file the data that will be plotted
 ofstream timeStatsFile_;
-
+// contain motion points
 map<int, int> distribution;
 
-Point2f mainCenter;         // variable for blob center tracking
-bool missedPlayer;          // flag for the player presence.
+// variable for blob center tracking
+Point2f mainCenter;
+// flag for the player presence
+bool missedPlayer;
 
-const int BUFFER = 32;          			// the object trail size
-boost::circular_buffer<cv::Point2f> pts(BUFFER);    	// The blob location history
+// the object trail size
+const int BUFFER = 32;
+// The blob location history
+boost::circular_buffer<cv::Point2f> pts(BUFFER);
 
-bool protonect_shutdown = false;        // Whether the running application should shut down.
+// Whether the running application should shut down
+bool protonect_shutdown = false;
 
 /* Interruption handler function. In this case, set the variable that controls the
  frame aquisition, breaking the loop, cleaning variables and exit the program elegantly*/
@@ -85,9 +90,9 @@ static void drawOptFlowMap(const Mat& flow, Mat& cflowmap, Mat& aux, int step, d
 	    line(cflowmap, Point(x,y), Point(cvRound(x+fxy.x), cvRound(y+fxy.y)), Scalar(255,0,0));
             circle(cflowmap, Point(x,y), 2, color, -1);
 
-	    if( (fabs(fxy.x)>5) && (fabs(fxy.y)>5) ) {		
+	    if( (fabs(fxy.x)>5) && (fabs(fxy.y)>5) ) {
 		line(aux, Point(x,y), Point(cvRound(x+fxy.x), cvRound(y+fxy.y)), Scalar(255,255,255));
-		circle(aux, Point(x,y), 2, Scalar(255,255,255), -1); 
+		circle(aux, Point(x,y), 2, Scalar(255,255,255), -1);
 	    }
         }
     }
@@ -113,33 +118,33 @@ void printDist() {
 /* main */
 int main(int argc, char** argv)
 try {
-    	// Turn on logging.
+  // Turn on logging.
 	rs::log_to_console(rs::log_severity::warn);
 	std::cout << "Starting..." << std::endl;
 
 	// realsense context
 	rs::context ctx;
-        cout << "There are " << ctx.get_device_count() << " connected RealSense devices." << endl << endl;
+  cout << "There are " << ctx.get_device_count() << " connected RealSense devices." << endl << endl;
 
 	// exit if not device is already connected
-        if (ctx.get_device_count() == 0) return EXIT_FAILURE;
+  if (ctx.get_device_count() == 0) return EXIT_FAILURE;
 
 	// rs defining device to be used
 	rs::device * dev = ctx.get_device(0);
 
-        cout << "Using device..."<< endl <<"   name: " << dev->get_name() << endl << "   serial number: "
-	<< dev->get_serial() << endl << "   firmware version: " << dev->get_firmware_version() 
+  cout << "Using device..."<< endl <<"   name: " << dev->get_name() << endl << "   serial number: "
+	<< dev->get_serial() << endl << "   firmware version: " << dev->get_firmware_version()
 	<< endl << endl;
 
 	// configure DEPTH to run at 60 frames per second
 	dev->enable_stream(rs::stream::depth, 640, 480, rs::format::z16, 60);
-        // configure RGB to run at 60 frames per second
+  // configure RGB to run at 60 frames per second
 	dev->enable_stream(rs::stream::color, 640, 480, rs::format::rgb8, 60);
 
 	// settings to improve the DEPTH image
 	rs::apply_depth_control_preset(dev, 3);
 
-        cout << "Device is warming up... " << endl << endl;
+  cout << "Device is warming up... " << endl << endl;
 
 	// start the device
 	dev->start();
@@ -148,10 +153,10 @@ try {
 	int codec = CV_FOURCC('M', 'J', 'P', 'G');
 	cv::VideoWriter out = VideoWriter("output.avi", codec, 10.0, Size(640, 480), true);
 
-    	// openCV frame containers
-    	Mat rgbmat, depthmat, depthAndRgb, regframe, aux, output;
+  // openCV frame containers
+  Mat rgbmat, depthmat, depthAndRgb, regframe, aux, output;
 
-    	// openCV frame definition for Optical Flow
+  // openCV frame definition for Optical Flow
 	Mat gray, flow, sflow, flow1, greenFlowMat;
 	UMat prevgray;
 
@@ -159,17 +164,19 @@ try {
 	auto startT = std::chrono::system_clock::now();
 
 	// blob detection image panel
-    	cv::namedWindow("mask", 1);
+  cv::namedWindow("mask", 1);
 
-    	cv::createTrackbar("hMin", "mask", &hMin, 256);
-    	cv::createTrackbar("sMin", "mask", &sMin, 256);
-    	cv::createTrackbar("vMin", "mask", &vMin, 256);
-    	cv::createTrackbar("hMax", "mask", &hMax, 256);
-    	cv::createTrackbar("sMax", "mask", &sMax, 256);
-    	cv::createTrackbar("vMax", "mask", &vMax, 256);
+  // track bar for the blob color detected image
+  cv::createTrackbar("hMin", "mask", &hMin, 256);
+  cv::createTrackbar("sMin", "mask", &sMin, 256);
+  cv::createTrackbar("vMin", "mask", &vMin, 256);
+  cv::createTrackbar("hMax", "mask", &hMax, 256);
+  cv::createTrackbar("sMax", "mask", &sMax, 256);
+  cv::createTrackbar("vMax", "mask", &vMax, 256);
 
-    	/* FEATURE VARIABLES */
-    	float meanDistance = 0;         // distance feature
+  // FEATURE VARIABLES
+  // distance feature
+  float meanDistance = 0;
 
 	// capture first 50 frames to allow camera to stabilize
 	for (int i = 0; i < 50; ++i) dev->wait_for_frames();
@@ -187,7 +194,8 @@ try {
 		// wait for new frame data
 		dev->wait_for_frames();
 
-		// RGB data acquisititimeon
+    // image frame acquisition
+		// RGB data acquisition
 		uchar *rgb = (uchar *) dev->get_frame_data(rs::stream::color);
 		// DEPTH data acquisition
 		uchar *depth = (uchar *) dev->get_frame_data(rs::stream::depth);
@@ -196,14 +204,15 @@ try {
 		// RGB
 		const uint8_t * rgb_frame = reinterpret_cast<const uint8_t *>(rgb);
 		cv::Mat rgb_ = cv::Mat(480, 640, CV_8UC3, (void*) rgb_frame);
-		cvtColor(rgb_, rgbmat, CV_BGR2RGB); // saving RGB data into (mat container)rgbmat
+    // saving RGB data into (mat container)rgbmat
+		cvtColor(rgb_, rgbmat, CV_BGR2RGB);
 
-                // DEPTH
+    // DEPTH
 		const uint8_t * depth_frame = reinterpret_cast<const uint8_t *>(depth);
 		Mat depth16(480, 2*640, CV_8U, (void*) depth_frame);
-                Mat depthM(depth16.size().height, depth16.size().width, CV_16UC1);
-                depth16.convertTo(depthM, CV_8UC3);
-		// min/max distance from the camera
+    Mat depthM(depth16.size().height, depth16.size().width, CV_16UC1);
+    depth16.convertTo(depthM, CV_8UC3);
+    // min/max distance from the camera
 		unsigned short min = 0.5, max = 3.5;
 		cv::Mat img0 = Mat::zeros(depthM.size().height, depthM.size().width, CV_8UC1);
 		cv::Mat depth_show;
@@ -215,7 +224,8 @@ try {
 		// OPTICAL FLOW
 		Mat frame(rgbmat.size(), rgbmat.type());
 		rgbmat.copyTo(frame);
-	   	cvtColor(frame, gray, COLOR_RGB2GRAY);
+    // readjusting original image to gray scale image
+	  cvtColor(frame, gray, COLOR_RGB2GRAY);
 
 		if (!prevgray.empty()) {
 			flow = Mat(gray.size(), CV_32FC2);
@@ -226,9 +236,9 @@ try {
 			// optical flow points
 			sflow = Mat(flow.size(), CV_8UC3);
 			aux = Mat::ones(flow.size(), CV_8U);
+      // obtain matrix (sflow, aux) with points detected for motion
 			drawOptFlowMap(flow, sflow, aux, 4, 1.5, Scalar(0, 255, 0));
 			//imshow("sflow", sflow); imshow("flow_aux", aux);
-
 			aux.convertTo(flow1, CV_8U);
 
 			// magnitude,angle
@@ -249,7 +259,7 @@ try {
 		trackUserByColor(frameToTrack, regframe);
 
 		// flow matrix AND green track
-     		if (flow1.cols > 0 && flow1.rows > 0) {
+    if (flow1.cols > 0 && flow1.rows > 0) {
 			bitwise_and(flow1, regframe, greenFlowMat);
 			circle(greenFlowMat, mainCenter, 5, Scalar(255, 255, 255), -1);
 
@@ -258,7 +268,7 @@ try {
 
 			// plotting
 			vector<Point2f> movingGreenPoints;	// movingGreenPoints vector
-			vector<float> movingGreenDepthPoints;   // movingGreenDepthPoints vector 
+			vector<float> movingGreenDepthPoints;   // movingGreenDepthPoints vector
 
 			int step = 16;
 			float mean_x = 0;
@@ -275,51 +285,58 @@ try {
 							mean_d += d;
 							movingGreenDepthPointsCount++;
 							timeStatsFile_ << mean_d << endl;
+              // save float point "d" extracted from depth matrix (to get the distance)
 							insertInDist(d);
+              // TODO verify "d" and extract distance from the camera to the object
 						}
 					}
 
+      // if vector with moving points exists
 			if (movingGreenPoints.size()) {
+        // average values of moving points
 				mean_x = mean(movingGreenPoints)[0];
 				mean_y = mean(movingGreenPoints)[1];
 
+        // TODO review if this value is reliable
 				// mean distance
 				if(movingGreenDepthPointsCount) mean_d /= movingGreenDepthPointsCount;
 
-				// normalization
+				// normalization (so x,y range will be -1 to 1)
 				float norm_mean_x = (2*mean_x)/greenFlowMat.rows - 1;
 				float norm_mean_y = (2*mean_y)/greenFlowMat.cols - 1;
 
 				// get the current time
 				auto currentT = std::chrono::system_clock::now();
+        // get the time duration for last operation
 				auto dur = currentT - startT;
-
+        // time in seconds
 				typedef std::chrono::duration<float> float_seconds;
 				auto secs = std::chrono::duration_cast<float_seconds>(dur);
 
-				timeStatsFile_ <<
-				secs.count() << ", " << norm_mean_x << ", " << norm_mean_y << ", " <<
-				mean_d << endl;
+        // data that will be saved to csv file to be plotted
+				timeStatsFile_ << secs.count() << ", "
+                        << norm_mean_x << ", "
+                        << norm_mean_y << ", "
+                        << mean_d << endl;
 
+        // distribution contains information about moving points in depth matrix
 				cout << "distribution" << endl;
 				printDist();
 				cleanDist();
 
+        // TODO method to use the “distance value, obtained” to plot it or send to bag file in order to communicate with other systems
+
+        // display matrix when object was detected by color and moving points
 				imshow("trackingColor-MatRGB",frameToTrack);
 				//imshow("depth16", depth16);
 				//imshow("depthmat", depthmat);
-				cout << "mean_d: " << mean_d << endl;
 
 			}
 
-     		}
+    }
 
 		// calling the plot
 		//system("/home/aerolabio/librealsense/examples/librealsense_feat/gnuplot/gp.sh");
-
-		// Update/show images
-		//imshow("trackingColor-MatRGB",frameToTrack);
-       	 	//imshow("depth", depthmat);
 
 		// save recorded video written in --> output.avi
 		out.write(frameToTrack);
